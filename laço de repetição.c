b2ace00768f7b08791e972b13fe670d07ff841b0
mateus09467 <160919207+mateus09467@users.noreply.github.com>
2024-03-07 13:54:42 -0300

    Add files via upload

---
+++ laço de repetição.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	int i;
+		setlocale(LC_ALL,"");
+
+	for(i=1;i<=10;i++){
+		printf("=\Número:%d\n ",i);
+	}
+
+	
+	
+	
+}
+int i;
+
---
+++ negatvo.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<locale.h>
+
+void verificarPositivoOuNegativo(numero){
+	if(numero==0){
+		printf("Zero é´neutro.");
+	}else if (numero>0){
+		printf("Positivo");
+	} else {
+		printf("Negativo");
+	}
+}
+int main(){
+	setlocale(LC_ALL,"");
+	
+		int numero;
+	printf("Digite um número:");
+	scanf("%i",&numero);
+	
+	verificarPositivoOuNegativo(numero);
+}
